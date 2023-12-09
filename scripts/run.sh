a4ab56a1f9aeb79a5002d905eb86009d420b3cff
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2023-12-09 07:13:30 -0500

    Update venv path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Activate the virtual environment and display our photos
-source ~/Digital-Photo-Frame/digital-photo-frame/venv/bin/activate
+source ~/Digital-Photo-Frame/digital-photo-frame/bin/activate
 python ~/Digital-Photo-Frame/scripts/run_photo_display.py
 deactivate
--- scripts/sync_photos.sh
+++ scripts/sync_photos.sh
@@ -2,6 +2,6 @@
 
 # Activate the virtual environment and sync our photos
 
-source ~/Digital-Photo-Frame/digital-photo-frame/venv/bin/activate
+source ~/Digital-Photo-Frame/digital-photo-frame/bin/activate
 python ~/Digital-Photo-Frame/scripts/sync_photos.py
 deactivate
