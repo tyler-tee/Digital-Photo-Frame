670066931ae99559f8c53cf01171cc79e9255506
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2023-12-09 07:44:35 -0500

    +abs paths

--- scripts/sync_photos.sh
+++ scripts/sync_photos.sh
@@ -2,6 +2,6 @@
 
 # Activate the virtual environment and sync our photos
 
-source ~/Digital-Photo-Frame/digital-photo-frame/bin/activate
-python ~/Digital-Photo-Frame/scripts/sync_photos.py
+source /home/pi/Digital-Photo-Frame/digital-photo-frame/bin/activate
+python /home/pi/Digital-Photo-Frame/scripts/sync_photos.py
 deactivate
